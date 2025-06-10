ac4654c060340fedf41b97f9ebd196d9a41f29be
Chamod-Fernando <chamodsanjula59@gmail.com>
2025-06-10 21:28:59 +0530

    final req res

--- BackEnd/src/main/java/com/chamo/BackEnd/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.chamo.BackEnd.repository;
-
-import com.chamo.BackEnd.entity.UserEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<UserEntity, String> {
-
-}
