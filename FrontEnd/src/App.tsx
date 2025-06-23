e2404e35a87b2d7003425a82289e97e19f891084
Chamod-Fernando <chamodsanjula59@gmail.com>
2025-06-23 19:12:59 +0530

    recorrection

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,12 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "@/components/pages/home";
+import SignIn from "@/components/pages/SignIn";
 
 const App = () => {
-
   return (
-    <>
-      <HomePage/>
-    </>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/SignIn" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
