40eae21bb9fd563e66f873576939eb8a97f6f5c2
Chamod-Fernando <chamodsanjula59@gmail.com>
2025-06-23 19:14:13 +0530

    recorrection

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "@/components/pages/home";
-import SignIn from "@/components/pages/SignIn";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/SignIn" element={<SignIn />} />
       </Routes>
     </BrowserRouter>
   );
