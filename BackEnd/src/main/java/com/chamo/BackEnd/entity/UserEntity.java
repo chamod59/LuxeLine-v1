edeff28cfcf031cf09e4b83d6d783acb06d99974
Chamod-Fernando <chamodsanjula59@gmail.com>
2025-06-23 19:36:52 +0530

    user Entity created

--- BackEnd/src/main/java/com/chamo/BackEnd/entity/UserEntity.java
+++ BackEnd/src/main/java/com/chamo/BackEnd/entity/UserEntity.java
@@ -1,4 +1,37 @@
 package com.chamo.BackEnd.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document(collection ="user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserEntity {
+
+@Id
+private String id;
+
+private String name;
+
+@Indexed(unique = true)
+private String email;
+
+@Indexed(unique = true)
+private String username;
+private String password;
+private String mobile;
+
+@CreatedDate
+private Date createdAt;
+private String verificationCode;
+private Boolean status;
+
 }
