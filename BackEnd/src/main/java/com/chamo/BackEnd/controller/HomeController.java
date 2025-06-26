1d811ffd4c46cbee808b93a5289d8e889a277e5c
Chamod-Fernando <chamodsanjula59@gmail.com>
2025-06-26 20:34:44 +0530

    Recorrected confilts

--- BackEnd/src/main/java/com/chamo/BackEnd/controller/HomeController.java
+++ BackEnd/src/main/java/com/chamo/BackEnd/controller/HomeController.java
@@ -2,24 +2,15 @@ package com.chamo.BackEnd.controller;
 
 import com.chamo.BackEnd.services.JWTService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HomeController {
     private final JWTService jwtService;
 
     public HomeController(JWTService jwtService) {
         this.jwtService = jwtService;
-    }
-
-    @GetMapping
-    public String getHello(){
-        return "hi";
-    }
-
-    @PostMapping("/login")
-    public String login(){
-        return null;
     }
 
     @GetMapping("/username")
